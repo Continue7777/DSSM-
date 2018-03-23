f4ea40c57bda6bb96884f6c20db8b3ffdcd72876
zfy <362418096@qq.com>
2018-03-23 11:21:30 +0800

    add predict and evaluate

--- input_data.py
+++ input_data.py
@@ -23,13 +23,15 @@ def generate_triple_pair(log_path):
     #写到excel里面
     # new_df.to_excel('triple_pair.xlsx')
 
+
 class Data_set:
     def __init__(self,data_path,train_percent=0.7):
         self.data_path = data_path
         self.df_triple = pd.read_excel(data_path)
         self.word_dict = self._get_word_dict()
         self.train_index_list,self.test_index_list = self._spilt_train_test_index(train_percent)
-    
+        self.main_question_list = self.get_main_question_list()
+        
     #获取词数
     def get_word_num(self):
         return len(self.word_dict)
@@ -68,7 +70,7 @@ class Data_set:
         return word_dict
     
     #通过索引把df转成词向量
-    def get_onehot_from_batch(self,index_list,column_name):
+    def get_one_hot_from_batch(self,index_list,column_name):
         batch_doc = list(self.df_triple.iloc[index_list][column_name])
         
         result = np.zeros((len(batch_doc),len(self.word_dict)))
@@ -78,3 +80,39 @@ class Data_set:
                     result[i,self.word_dict[w]] = 1
 
         return result 
+    
+    # 获取一个句子的onehot
+    def get_one_hot_from_sentence(self,sentence):
+        #转化到unicode编码
+        if isinstance(sentence,str):
+            sentence = sentence.decode('utf-8')
+        
+        result = np.zeros((1,len(self.word_dict)))
+    
+        for w in sentence:
+            if w in self.word_dict:
+                result[0,self.word_dict[w]] = 1
+        return result
+    
+    # 获取所有主问题的onehot
+    def get_one_hot_from_main_question(self):
+        l = self.main_question_list
+        
+        result = np.zeros((len(l),len(self.word_dict)))
+        for i,s in enumerate(l):
+            for j,w in enumerate(s):
+                if w in self.word_dict:
+                    result[i,self.word_dict[w]] = 1
+        return result
+    
+    #根据label_Index获取main_question
+    def get_main_question_from_label_index(self,label_index):
+        return self.main_question_list[label_index]
+        
+        
+    #获取该pair下所有的主问题
+    def get_main_question_list(self):
+        l = list(self.df_triple['main_question'])
+        l.extend(list(self.df_triple['wrong_question']))
+        l = list(set(l))
+        return l
--- triple_compare.py
+++ triple_compare.py
@@ -17,9 +17,9 @@ flags.DEFINE_integer('epoch_num', 10, 'Number of epoch.')
 flags.DEFINE_bool('gpu', 0, "Enable GPU or not")
 
 # 读取数据
+from data_input import Data_set
+data_set = Data_set(data_path='triple_pair.xlsx')
 
-data_path = 'triple_pair.xlsx'
-data_set = Data_set(data_path)
 train_size, test_size = data_set.get_train_test_size()
 train_index_list = data_set.train_index_list
 test_index_list = data_set.test_index_list
@@ -30,8 +30,7 @@ norm, epsilon = False, 0.001
 
 TRIGRAM_D = data_set.get_word_num()
 MAIN_QUESTION_NUM = data_set.get_main_question_num()
-# negative sample
-# query batch size
+
 query_BS = 100
 
 L1_N = 400
@@ -184,17 +183,17 @@ with tf.name_scope('Predict'):
     pred_query_norm = tf.tile(tf.sqrt(tf.reduce_sum(tf.square(query_y), 1, True)), [MAIN_QUESTION_NUM, 1])
     pred_doc_norm = tf.sqrt(tf.reduce_sum(tf.square(doc_positive_y), 1, True))
 
-    pred_prod = tf.reduce_sum(tf.multiply(query_y, doc_y), 1, True)
-    pred_norm_prod = tf.multiply(query_norm, doc_norm)
+    pred_prod = tf.reduce_sum(tf.multiply(query_y, doc_positive_y), 1, True)
+    pred_norm_prod = tf.multiply(pred_query_norm, pred_doc_norm)
 
     # cos_sim_raw = query * doc / (||query|| * ||doc||)
-    pred_cos_sim_raw = tf.truediv(prod, norm_prod)
+    pred_cos_sim_raw = tf.truediv(pred_prod, pred_norm_prod)
 
-    pred_cos_sim = tf.transpose(tf.reshape(tf.transpose(cos_sim_raw), [MAIN_QUESTION_NUM, 1]))
+    pred_cos_sim = tf.transpose(tf.reshape(tf.transpose(pred_cos_sim_raw), [MAIN_QUESTION_NUM, 1]))
     
-    pred_prob = tf.nn.softmax(cos_sim)
+    pred_prob = tf.nn.softmax(pred_cos_sim)
     # 只取第一列，即正样本列概率。
-    pred_label = tf.argmax(pred_prob)
+    pred_label = tf.argmax(pred_prob,1)[0]
     
 merged = tf.summary.merge_all()
 
@@ -210,9 +209,9 @@ with tf.name_scope('Train'):
 
 def pull_all(index_list):
     #该地方插入函数，把query_iin，doc_positive_in,doc_negative_in转化成one_hot，再转化成coo_matrix
-    query_in = data_set.get_onehot_from_batch(index_list,'query')
-    doc_positive_in = data_set.get_onehot_from_batch(index_list,'main_question')
-    doc_negative_in = data_set.get_onehot_from_batch(index_list,'wrong_question')
+    query_in = data_set.get_one_hot_from_batch(index_list,'query')
+    doc_positive_in = data_set.get_one_hot_from_batch(index_list,'main_question')
+    doc_negative_in = data_set.get_one_hot_from_batch(index_list,'wrong_question')
     
     query_in = coo_matrix(query_in)
     doc_positive_in = coo_matrix(doc_positive_in)
@@ -259,14 +258,13 @@ def feed_dict(train_index_list,test_index_list,on_training, Train, batch_id):
     return {query_batch: query_in, doc_positive_batch: doc_positive_in, doc_negative_batch: doc_negative_in,
             on_train: on_training}
 
-
 config = tf.ConfigProto() 
 config.gpu_options.allow_growth = True
 if not FLAGS.gpu:
     config = tf.ConfigProto(device_count= {'GPU' : 0},allow_soft_placement=True)
 
 # 创建一个Saver对象，选择性保存变量或者模型。
-# saver = tf.train.Saver()
+saver = tf.train.Saver()
 with tf.Session(config=config) as sess:
 # with tf.Session() as sess:
     print "variable initial"
@@ -312,5 +310,52 @@ with tf.Session(config=config) as sess:
               (epoch_id, epoch_loss,end - start,epoch_acc))
 
     # 保存模型
-#     save_path = saver.save(sess, "model/model_1.ckpt")
-#     print("Model saved in file: ", save_path)
+    save_path = saver.save(sess, "model/model_1.ckpt")
+    print("Model saved in file: ", save_path)
+
+def feed_evaluate_dict(sentence,on_training=True):
+    """
+    input: data_sets is a dict and the value type is numpy
+    describe: to match the text classification the data_sets's content is the doc in df
+    """
+    #该地方插入函数，把query_iin，doc_positive_in,doc_negative_in转化成one_hot，再转化成coo_matrix
+    query_in = data_set.get_one_hot_from_sentence(sentence)
+    doc_positive_in = data_set.get_one_hot_from_main_question()
+    doc_negative_in = np.ones((1,data_set.get_word_num()))
+    
+    query_in = coo_matrix(query_in)
+    doc_positive_in = coo_matrix(doc_positive_in)
+    doc_negative_in = coo_matrix(doc_negative_in)
+    
+    query_in = tf.SparseTensorValue(
+        np.transpose([np.array(query_in.row, dtype=np.int64), np.array(query_in.col, dtype=np.int64)]),
+        np.array(query_in.data, dtype=np.float),
+        np.array(query_in.shape, dtype=np.int64))
+    doc_positive_in = tf.SparseTensorValue(
+        np.transpose([np.array(doc_positive_in.row, dtype=np.int64), np.array(doc_positive_in.col, dtype=np.int64)]),
+        np.array(doc_positive_in.data, dtype=np.float),
+        np.array(doc_positive_in.shape, dtype=np.int64))
+    doc_negative_in = tf.SparseTensorValue(
+        np.transpose([np.array(doc_negative_in.row, dtype=np.int64), np.array(doc_negative_in.col, dtype=np.int64)]),
+        np.array(doc_negative_in.data, dtype=np.float),
+        np.array(doc_negative_in.shape, dtype=np.int64))
+    
+    return {query_batch: query_in, doc_positive_batch: doc_positive_in, doc_negative_batch: doc_negative_in,
+            on_train: on_training}
+
+def predict_label(sentence):
+    saver = tf.train.Saver()
+
+    #写一个函数查看输入query和输出类别
+    config = tf.ConfigProto() 
+    config.gpu_options.allow_growth = True
+    if not FLAGS.gpu:
+        config = tf.ConfigProto(device_count= {'GPU' : 0},allow_soft_placement=True)
+    with tf.Session(config=config) as sess:     
+        saver.restore(sess, "model/model_1.ckpt")
+        print "Model restored."
+        print sess.run(query_l1 ,feed_dict=feed_evaluate_dict(sentence))
+        pred_prob_v,pred_label_v = sess.run([pred_prob,pred_label],feed_dict=feed_evaluate_dict(sentence))
+        pred_main_question = data_set.get_main_question_from_label_index(pred_label_v)
+        print sentence,pred_main_question,pred_label_v
+                                        
